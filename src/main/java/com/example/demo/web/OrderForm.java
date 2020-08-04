46e44a60feb582ce30554d60af656af79350fa55
TNAJI Khalid <khalidtnaji@gmail.com>
2020-08-04 17:06:23 +0200

    ready to be used in gitpod

--- src/main/java/com/example/demo/web/OrderForm.java
+++ src/main/java/com/example/demo/web/OrderForm.java
@@ -11,6 +11,30 @@ public class OrderForm {
 	private Client client;
 	private com.example.demo.web.Caddy caddy;
 	private float totalAmount;
+
+	public Client getClient( ) {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Caddy getCaddy( ) {
+		return caddy;
+	}
+
+	public void setCaddy(Caddy caddy) {
+		this.caddy = caddy;
+	}
+
+	public float getTotalAmount( ) {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(float totalAmount) {
+		this.totalAmount = totalAmount;
+	}
 }
 
 @Data
@@ -18,6 +42,30 @@ class Caddy {
 	public String name;
 	public Map<Integer, ProductItem> items;
 	public Client client;
+
+	public String getName( ) {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Map<Integer, ProductItem> getItems( ) {
+		return items;
+	}
+
+	public void setItems(Map<Integer, ProductItem> items) {
+		this.items = items;
+	}
+
+	public Client getClient( ) {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
 }
 
 @Data
@@ -25,4 +73,28 @@ class ProductItem {
 	public Product product;
 	public Integer quantity;
 	public Integer price;
+
+	public Product getProduct( ) {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Integer getQuantity( ) {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Integer getPrice( ) {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
 }
